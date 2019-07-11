b4e226934a7557776c28a956b9a784a53e91bb43
anujssmishra <40787130+anujssmishra@users.noreply.github.com>
2019-07-11 07:21:03 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -93,7 +93,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'anujssmishra@gmail.com'
-EMAIL_HOST_PASSWORD = 'Alpha.Gmail'
+EMAIL_HOST_PASSWORD = 'your password'
 
 
 # Password validation
