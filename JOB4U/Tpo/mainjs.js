04d88c4e331e9717b7bed1a47f27b137681686ab
Yash_Sampat <yashsampat23154@gmail.com>
2019-09-02 20:10:17 +0530

    Link added to Images

--- JOB4U/Tpo/mainjs.js
+++ JOB4U/Tpo/mainjs.js
@@ -1,9 +1,17 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "400px";
+  document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white" ;
 }
+
+$('body').on('click', function(){
+  if( parseInt( $('#mySidenav').css('width') ) > 0 ){
+    closeNav();
+  }
+});
