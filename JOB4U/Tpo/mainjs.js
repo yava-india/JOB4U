74b67aba92da1feca8d84ca4085b7375cd1200a6
Yash_Sampat <yashsampat23154@gmail.com>
2019-09-02 16:21:05 +0530

    New Tpo Template

---
+++ JOB4U/Tpo/mainjs.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "400px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
