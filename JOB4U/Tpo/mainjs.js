b61f703ab23395bcafe9c4fd2ee255c9a7044a11
Yash_Sampat <yashsampat23154@gmail.com>
2019-09-08 11:57:47 +0530

    ReachUs added along with animations

--- JOB4U/Tpo/mainjs.js
+++ JOB4U/Tpo/mainjs.js
@@ -8,11 +8,13 @@ function openNav() {
   document.getElementById("our-recruiters").style.opacity = "0.4";
   document.getElementById("image-gallery").style.opacity = "0.4";
   document.getElementById("video-gallery").style.opacity = "0.4";
+  document.getElementById("reach-us").style.opacity = "0.4";
+  document.getElementById("cuform").style.width = "0%";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0px";
   document.body.style.backgroundColor = "black" ;
   document.getElementById("myVideo").style.opacity = "1";
   document.getElementById("containerinVideo").style.opacity = "1";
@@ -20,7 +22,36 @@ function closeNav() {
   document.getElementById("our-recruiters").style.opacity = "1";
   document.getElementById("image-gallery").style.opacity = "1";
   document.getElementById("video-gallery").style.opacity = "1";
+  document.getElementById("reach-us").style.opacity = "1";
 }
+
+function openForm() {
+  // document.getElementById("cuform").style.width = "40%";
+  document.getElementById("cuform").style.display = "block";
+  document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+  document.getElementById("myVideo").style.opacity = "0.4";
+  document.getElementById("containerinVideo").style.opacity = "0.4";
+  document.getElementById("upcoming-drives").style.opacity = "0.4";
+  document.getElementById("our-recruiters").style.opacity = "0.4";
+  document.getElementById("image-gallery").style.opacity = "0.4";
+  document.getElementById("video-gallery").style.opacity = "0.4";
+  document.getElementById("reach-us").style.opacity = "0.4";
+  document.getElementById("mySidenav").style.width = "0px";
+}
+
+function closeForm() {
+  // document.getElementById("cuform").style.width = "0%";
+  document.getElementById("cuform").style.display = "none";
+  document.body.style.backgroundColor = "black" ;
+  document.getElementById("myVideo").style.opacity = "1";
+  document.getElementById("containerinVideo").style.opacity = "1";
+  document.getElementById("upcoming-drives").style.opacity = "1";
+  document.getElementById("our-recruiters").style.opacity = "1";
+  document.getElementById("image-gallery").style.opacity = "1";
+  document.getElementById("video-gallery").style.opacity = "1";
+  document.getElementById("reach-us").style.opacity = "1";
+}
+
 
 $('body').on('click', function(){
   if( parseInt( $('#mySidenav').css('width') ) > 0 ){
